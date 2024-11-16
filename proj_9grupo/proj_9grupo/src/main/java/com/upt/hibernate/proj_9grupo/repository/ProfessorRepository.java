57b1a685ad102a642c5bf150c73f74df233c35d4
Francisco Carneiro <franciscomcarneiro2013@gmail.com>
2024-11-16 23:56:36 +0000

    Add files via upload
    Implementação de código nas classes AlunosRepository e ProfessorRepository

--- proj_9grupo/proj_9grupo/src/main/java/com/upt/hibernate/proj_9grupo/repository/ProfessorRepository.java
+++ proj_9grupo/proj_9grupo/src/main/java/com/upt/hibernate/proj_9grupo/repository/ProfessorRepository.java
@@ -1,13 +1,15 @@
 package com.upt.hibernate.proj_9grupo.repository;
 
 import com.upt.hibernate.proj_9grupo.model.Professor;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 	
-	
-	
-	
+	List <Professor> findByNome(String nome);
+	List <Professor> findByNumeroProfessor(int numProfessor);
+	List <Professor> findByDisciplina(String disciplina);
+	boolean existByEmail(String email);
 }
