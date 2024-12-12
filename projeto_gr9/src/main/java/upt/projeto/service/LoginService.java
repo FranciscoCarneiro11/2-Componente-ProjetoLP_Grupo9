e3a94cb23e1fcf39e335948c962438da97d1456b
Francisco Carneiro <franciscomcarneiro2013@gmail.com>
2024-12-12 22:39:33 +0000

    Add files via upload
    Implementação da parte do Login e do Criar Conta

---
+++ projeto_gr9/src/main/java/upt/projeto/service/LoginService.java
@@ -0,0 +1,29 @@
+package upt.projeto.service;
+
+import upt.projeto.model.Login;
+import upt.projeto.model.RespostaLogin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class LoginService {
+	private static final String BASE_URL = "http://localhost:8080/api";
+    private RestTemplate restTemplate = new RestTemplate();
+    
+    public boolean login(String email, String password) {
+        String url = BASE_URL + "/login"; 
+        Login loginRequest = new Login(email, password);
+
+        ResponseEntity<RespostaLogin> response = restTemplate.postForEntity(url, loginRequest, RespostaLogin.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+        	RespostaLogin loginResponse = response.getBody();
+            return loginResponse != null && loginResponse.isSuccess(); 
+        } else {
+            System.out.println("Login falhou: " + response.getStatusCode());
+            return false; 
+        }
+    }
+    
+    
+    
+}
